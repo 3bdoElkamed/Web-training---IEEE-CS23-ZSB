6f70f4de027c08e0d6bab244e6433f1c71e06b28
Abdulrahman Ibrahim <43120443+3bdoElkamed@users.noreply.github.com>
2023-03-06 22:41:01 +0200

    Task-8 Js-part-1

---
+++ Task-8 JS-part-1/assignments-lesson-from-1-to-9/Assignment-2,3,4,5,6/main.js
@@ -0,0 +1,49 @@
+// Assigment 2
+document.write("<h1>IEEE</h1>");
+document.querySelector("h1").style.color = "blue";
+document.querySelector("h1").style.fontSize = "80px";
+document.querySelector("h1").style.textAlign = "center";
+document.querySelector("h1").style.fontFamily = "Arial";
+
+
+// Assignmet 3
+console.log(
+  "%cElzero %cWeb %cSchool",
+  "color: red; font-size: 40px;",
+  "color: green; font-size: 40px; font-weight: bold;",
+  "color: blue; font-size: 40px;"
+);
+
+
+// Assignment 4
+console.group("Group One");
+console.log("Messege One");
+console.log("Messege Two");
+console.group("Child group");
+console.log("Messege One");
+console.log("Messege Two");
+console.group("Grand child group");
+console.log("Messege One");
+console.log("Messege Two");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("Group Two");
+console.log("Messege One");
+console.log("Messege Two");
+
+
+// Assignment 5
+console.table(["Elzero", "Ahmed", "Sameh", "Gamal", "Aya"]);
+
+
+// Assignment 6
+// First:
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+// Second:
+/*
+  console.log("Iam In Console");
+  document.write("Iam In Page");
+*/
